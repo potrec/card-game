3d8c6e1a1bfa11c4b0de53359fe0e261e1349bb8
Maciej Potręć <potrec123@proton.me>
2025-02-07 02:21:28 +0100

    feat: add working drag&drop for cards

--- card-game/Assets/Scripts/BasicCard.cs
+++ card-game/Assets/Scripts/BasicCard.cs
@@ -1,14 +1,38 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class BasicCard : MonoBehaviour
+public class BasicCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public CardSO cardData;
     private Image cardImage;
+    private Transform parentAfterDrag;
     
     public void Initialize() {
         GetComponent<Image>().sprite = cardData.cardImage;
     }
     
-    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        transform.rotation = Quaternion.identity;
+        DeckManager.Instance.handCards.Remove(cardData);
+        DeckManager.Instance.currentCard = cardData;
+        Debug.Log($"Dragged {cardData.cardName}");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        DeckManager.Instance.tableCards.Add(cardData);
+        transform.SetParent(DeckManager.Instance.handUI);
+        DeckManager.Instance.currentCard = null;
+        DeckManager.Instance.UpdateVisuals();
+    }
 }
--- card-game/Assets/Scripts/DeckManager.cs
+++ card-game/Assets/Scripts/DeckManager.cs
@@ -1,27 +1,60 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DeckManager : MonoBehaviour
 {
-    [SerializeField] private CardSO[] deckCards;
-    [SerializeField] private CardSO[] handCards;
-    [SerializeField] private CardSO[] discardPile;
+    public static DeckManager Instance { get; private set; }
+    
+    public List<CardSO> deckCards;
+    public List<CardSO> handCards;
+    public List<CardSO> tableCards;
+    public List<CardSO> discardPile;
+    
+    public CardSO currentCard { get; set; } = null;
+    
     [SerializeField] private Transform basicCardPrefab;
     [SerializeField] private Transform deckUI;
-
-    public int cardRotationInDeck = 5;
     
+    public Transform handUI;
+
+    public float cardRotationInDeck = 2.5f;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     private void Start()
     {
-        float cardRotation = cardRotationInDeck * deckCards.Length/2;
-        for (int i = 0; i < deckCards.Length; i++)
+        UpdateVisuals();
+    }
+    
+    private void ShuffleDeck()
+    {
+        
+    }
+    
+    public void UpdateVisuals()
+    {
+        foreach (Transform child in deckUI)
+        {
+            // if (child == basicCardPrefab) continue;
+            Destroy(child.gameObject);
+        }
+        
+        float cardRotation = cardRotationInDeck * (handCards.Count-1)/2;
+        for (int i = 0; i < handCards.Count; i++)
         {
             var card = Instantiate(basicCardPrefab, deckUI);
             var cardScript = card.GetComponent<BasicCard>();
             
-            cardScript.cardData = deckCards[i];
+            cardScript.cardData = handCards[i];
             cardScript.Initialize();
             card.localRotation = Quaternion.Euler(0, 0, cardRotation-i*cardRotationInDeck);
         }
     }
-    
+
 }
